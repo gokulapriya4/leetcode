30bf7e6138d35e447f5adfe0ef3a7596eb0ed34a
Gokula Priya <gokulapriya4@gmail.com>
2020-06-25 09:52:17 +0530

    initial commit

---
+++ src/main/java/com/priya/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,36 @@
+package main.java.com.priya.leetcode;
+
+
+class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+
+        int currentIndexToPlace = 1;
+        for (int i = 1; i < nums.length; i++) {
+            boolean isDuplicate = false;
+
+            for (int j = 0; j < i; j++) {
+                if(nums[j] == nums[i]) {
+                    isDuplicate = true;
+                }
+            }
+
+            if(!isDuplicate) {
+                nums[currentIndexToPlace] = nums[i];
+                currentIndexToPlace++;
+            }
+        }
+
+        for (int i = 0; i < currentIndexToPlace; i++) {
+            System.out.println(nums[i]);
+        }
+        return currentIndexToPlace;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
+        int[] nums = {1, 1, 2};
+        System.out.println("Fianl Result: " + obj.removeDuplicates(nums));
+    }
+
+}
