dd08baa9b94e034f2cf4963b98c8a3250522717c
Gokula Priya <gokulapriya4@gmail.com>
2020-06-25 16:14:44 +0530

    adding poblems

---
+++ src/main/java/com/priya/leetcode/SingleNumber.java
@@ -0,0 +1,29 @@
+package main.java.com.priya.leetcode;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+public class SingleNumber {
+
+    //Chsen this approach since they asked not to use extra memory
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                return nums[i];
+            }
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+        SingleNumber singleNumber = new SingleNumber();
+        System.out.println(singleNumber.singleNumber(nums));
+    }
+
+}
