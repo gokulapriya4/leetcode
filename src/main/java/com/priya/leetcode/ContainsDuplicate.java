77205af9c02672cf025269ad6d1cf06fa06e85c9
Gokula Priya <gokulapriya4@gmail.com>
2020-06-25 16:24:52 +0530

    adding poblems

---
+++ src/main/java/com/priya/leetcode/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package main.java.com.priya.leetcode;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        int distinctElementsSize = Arrays.stream(nums).boxed().distinct().collect(Collectors.toList()).size();
+        return nums.length != distinctElementsSize;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+
+        System.out.println(containsDuplicate.containsDuplicate(nums));
+    }
+}
