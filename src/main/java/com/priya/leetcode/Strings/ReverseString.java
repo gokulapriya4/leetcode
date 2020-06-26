a506b2ce4ca59a3da02f2e0d370770def2bc87a9
Gokula Priya <gokulapriya4@gmail.com>
2020-06-26 09:16:47 +0530

    adding poblems

---
+++ src/main/java/com/priya/leetcode/Strings/ReverseString.java
@@ -0,0 +1,22 @@
+package main.java.com.priya.leetcode.Strings;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char tempChar = s[i];
+            s[i] = s[j];
+            s[j] = tempChar;
+        }
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ReverseString reverseString = new ReverseString();
+        char[] s = {'p', 'r', 'i', 'y', 'a'};
+        reverseString.reverseString(s);
+    }
+}
