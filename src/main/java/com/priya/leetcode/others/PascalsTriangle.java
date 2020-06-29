1e74874794ea62ce18f27bf3a426eea727124a9e
Gokula Priya <gokulapriya4@gmail.com>
2020-06-29 14:27:07 +0530

    adding poblems

---
+++ src/main/java/com/priya/leetcode/others/PascalsTriangle.java
@@ -0,0 +1,19 @@
+package main.java.com.priya.leetcode.others;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/601/
+public class PascalsTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PascalsTriangle obj = new PascalsTriangle();
+        System.out.println(obj.generate(5));
+    }
+}
