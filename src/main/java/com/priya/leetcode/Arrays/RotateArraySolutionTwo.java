7a8215612b7ffbec658c3f49362f5a56fd8e6357
Gokula Priya <gokulapriya4@gmail.com>
2020-07-03 11:01:41 +0530

    adding poblems

--- src/main/java/com/priya/leetcode/Arrays/RotateArraySolutionTwo.java
+++ src/main/java/com/priya/leetcode/Arrays/RotateArraySolutionTwo.java
@@ -26,11 +26,6 @@ public class RotateArraySolutionTwo {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        //7 1 2 3 4 5 6
-        //6 7 1 2 3 4 5
-        //5 6 7 1 2 3 4
-
-        //7 6 5 4 3 2 1
         int k = 3;
 
         RotateArraySolutionTwo obj = new RotateArraySolutionTwo();
