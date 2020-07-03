32a7c88233506dd2e245feefd5ffbfb94864f126
Gokula Priya <gokulapriya4@gmail.com>
2020-07-03 11:01:20 +0530

    adding poblems

--- src/main/java/com/priya/leetcode/Arrays/RotateArraySolutionTwo.java
+++ src/main/java/com/priya/leetcode/Arrays/RotateArraySolutionTwo.java
@@ -4,12 +4,35 @@ package main.java.com.priya.leetcode.Arrays;
 public class RotateArraySolutionTwo {
 
     public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, nums.length - 1);
+    }
 
+    public int[] reverseArray(int[] nums, int startIndex, int endIndex) {
+        int temp;
+
+        while (endIndex > startIndex) {
+            temp = nums[startIndex];
+            nums[startIndex] = nums[endIndex];
+            nums[endIndex] = temp;
+
+            startIndex++;
+            endIndex--;
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        //7 1 2 3 4 5 6
+        //6 7 1 2 3 4 5
+        //5 6 7 1 2 3 4
+
+        //7 6 5 4 3 2 1
         int k = 3;
+
         RotateArraySolutionTwo obj = new RotateArraySolutionTwo();
         obj.rotate(nums, k);
         for (int i = 0; i < nums.length; i++) {
