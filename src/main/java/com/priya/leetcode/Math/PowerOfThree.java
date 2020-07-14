5fc6ac6b11cc6cc1e203ee2f205b0fb1da65d39d
Gokula Priya <gokulapriya4@gmail.com>
2020-07-14 14:55:23 +0530

    adding poblems

---
+++ src/main/java/com/priya/leetcode/Math/PowerOfThree.java
@@ -0,0 +1,13 @@
+package main.java.com.priya.leetcode.Math;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/745/
+public class PowerOfThree {
+    public static boolean isPowerOfThree(int n) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 45;
+        System.out.println(isPowerOfThree(n));
+    }
+}
