5fed8129f41c2c274397bbb310e52f4724839c3c
Gokula Priya <gokulapriya4@gmail.com>
2020-07-15 13:00:54 +0530

    adding poblems

--- src/main/java/com/priya/leetcode/Arrays/MinimumSizeSubArraySumBetterSolution.java
+++ src/main/java/com/priya/leetcode/Arrays/MinimumSizeSubArraySumBetterSolution.java
@@ -2,9 +2,25 @@ package main.java.com.priya.leetcode.Arrays;
 
 //https://leetcode.com/problems/minimum-size-subarray-sum/
 public class MinimumSizeSubArraySumBetterSolution {
-
+    //Got help from Leetcode
     public int minSubArrayLen(int s, int[] nums) {
-        return 1;
+        int sum = 0, k = 0, result = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum >= s) {
+                while (true) {
+                    if (sum - nums[k] >= s) {
+                        sum = sum - nums[k];
+                        k++;
+                    } else {
+                        result = Math.min(result, i - k + 1);
+                        break;
+                    }
+                }
+            }
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 
     public static void main(String[] args) {
