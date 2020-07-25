cebb5f8875ef34eb376799f5bd739d53bd51a99b
gokula <gokula.priya@nascentinfo.com>
2020-07-25 16:05:38 +0530

    adding problems

--- src/main/java/com/priya/leetcode/Arrays/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/priya/leetcode/Arrays/BestTimeToBuyAndSellStock.java
@@ -3,14 +3,23 @@ package main.java.com.priya.leetcode.Arrays;
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
 public class BestTimeToBuyAndSellStock {
 
-    //ToDO
     public int maxProfit(int[] prices) {
-        return 1;
+        int maxProfit = 0, buy = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (i == prices.length - 1 || prices[i] > prices[i + 1]) {
+                maxProfit += prices[i] - buy;
+                buy = Integer.MAX_VALUE;
+            }
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
         BestTimeToBuyAndSellStock obj = new BestTimeToBuyAndSellStock();
         int[] prices = {7, 6, 4, 3, 1};
-        obj.maxProfit(prices);
+        System.out.println(obj.maxProfit(prices));
     }
 }
